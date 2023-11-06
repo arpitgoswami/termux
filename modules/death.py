e6578f73a47ec8027d01f6f7063a76b6ccc192d2
Arpit Goswami <71710858+arpitgoswami@users.noreply.github.com>
2023-11-06 01:57:03 -0800

    Add files via upload

---
+++ modules/death.py
@@ -0,0 +1,4 @@
+def death(a, b, c):
+    print(20, end="")
+    print(c + (b+a))
+    return
---
+++ modules/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup (
+name = 'death',
+version = '1.0',
+description = 'my new python module',
+author = 'Arpit',
+author_email = 'anonymous@loonycorn.com',
+url = 'loony.com',
+py_modules = ['death']
+)
